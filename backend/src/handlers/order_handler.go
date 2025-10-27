e05e00a82d55008f5dc10953cb9b3fa1d9125ca2
Lucas Faria <lucasfaria@outlook.pt>
2025-10-27 17:13:17 +0000

    First API endpoint (order)

---
+++ backend/src/handlers/order_handler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"app/models"
+	"errors"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type OrderHandler struct{
+	DB *gorm.DB
+}
+
+func (h *OrderHandler) GetOrderByID(c *gin.Context){
+	id := c.Param("id")
+	var order models.Orders
+	result := h.DB.First(&order, id)
+
+	//check if there was an error with the database request
+    if result.Error != nil {
+        if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+            c.JSON(http.StatusNotFound, gin.H{"error": "Order not found"})
+        } else {
+            c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
+        }
+        
+    } else{
+		c.JSON(http.StatusOK,order)
+	}
+	
+}
--- backend/src/main.go
+++ backend/src/main.go
@@ -1,25 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "github.com/gin-contrib/cors"
 import (
-  "gorm.io/driver/postgres"
-  "gorm.io/gorm"
+	"app/routes"
+	"fmt"
+	"os"
+	"time"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
-
-import "os"
-import "fmt"
-import "time"
 
 
 func main() {
   dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT") )
-  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-  // Simple raw query to test connection
-  sqlDB, err := db.DB()
-
-  err = sqlDB.Ping()
+  db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
   router := gin.Default()
 
@@ -36,17 +31,8 @@ func main() {
     MaxAge: 12 * time.Hour,
   }))
 
-  router.GET("/pong", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
-  router.GET("/", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "DS is awesome!",
-      "connected": err,
-    })
-  })
+  //registers the routes
+  routes.RegisterRoutes(router, db)
 
-  router.Run() // listens on 0.0.0.0:8080 by default
+  router.Run(":8080") // listens on 0.0.0.0:8080 by default
 }
